5f1d72c2d11411920a551430794591046ce24387
Jan <janjovellanos@gmail.com>
2023-09-27 01:11:25 -0700

    calc bill amount by state

--- utils/helpers.py
+++ utils/helpers.py
@@ -3,12 +3,12 @@ import random
 # Parses incoming list of lines from .txt file
 def message_parser(message):
     # data = {'#': sample_data_df['#'].max() + 1}
-    # Preset bill amount
+    # Preset bill amount and random '#' value
     data = {
         'bill_amount': 1234,
-        '#': random.randint(100,999)
+        '#': random.randint(201,999)
     }
-    # Loop through list and extract required fields
+    # Loop through list and extract fields for new record we are adding into output files
     for segment in message:
         if segment.startswith('MSH|'):
             data['message_type'] = segment.split('|')[8].replace('^', '-')
@@ -18,6 +18,8 @@ def message_parser(message):
             data['patient_last_name'] = fields[5].split('^')[0]
             data['patient_middle_name'] = fields[5].split('^')[2]
             data['patient_address_1'] = fields[11].split('^')[0]
+            data['patient_city'] = fields[11].split('^')[2]
             data['patient_state'] = fields[11].split('^')[3]
+            data['patient_zip'] = fields[11].split('^')[4]
             data['account_number'] = fields[3]
     return [data]
